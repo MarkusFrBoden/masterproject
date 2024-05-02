d6dcb14daecb743ea9eee03ecbf6702d7763107b
Markus Boden <markus_boden@outlook.de>
2024-05-02 23:19:13 +0200

    comment out redirect path / for github pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,10 +12,10 @@ import LoginView from '../views/LoginView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
+    /* {
       path: '/',
       redirect: { name: 'login' }
-    },
+    }, */
     {
       path: '/login',
       name: 'login',
